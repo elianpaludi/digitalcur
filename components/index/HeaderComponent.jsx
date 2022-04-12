3e242e6f10e9aee7c5724497112c1c1cd6944020
Elian <elianpaludi@gmail.com>
2022-04-11 23:59:55 -0300

    aaa

---
+++ components/index/HeaderComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header.module.scss'
+
+const HeaderComponent = () => {
+  return (
+    <div className={Header.container}>
+
+    </div>
+  )
+}
+
+export default HeaderComponent
