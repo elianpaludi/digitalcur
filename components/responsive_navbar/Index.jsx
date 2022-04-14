dcc7c454484e5b8041484520093b084ccfab0e70
Elian <elianpaludi@gmail.com>
2022-04-13 22:07:57 -0300

    navbar responsive

---
+++ components/responsive_navbar/Index.jsx
@@ -0,0 +1,12 @@
+import Style from './ResponsiveNav.module.scss'
+
+export default function Index() {
+  return (
+
+    <div className={Style.responsiv}>
+        <nav className={Style.container}>
+            
+        </nav>
+    </div>
+  )
+}
